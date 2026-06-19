2eda545c8e5b21b8f821cede89d8dfa4f873a3d8
marionmorel <marion.mrl@live.fr>
2026-06-19 10:36:16 +0100

    Tasks 1 - 3

--- script.py
+++ script.py
@@ -4,4 +4,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 import seaborn as sns
-import codecademylib3
+
+# Task 1
+spotify_data = pd.read_csv('spotify_data.csv')
+
+# Task 2
+print(spotify_data.head())
+
+# Task 3
+song_tempos = spotify_data['tempo']
